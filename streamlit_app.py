5ae7b06abd45aa3d18715597ba07785563beb6b9
Fernando Meza <43585972+mezaf@users.noreply.github.com>
2023-07-11 10:29:13 -0300

    adding choice selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,11 @@ fruits_to_show = fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
