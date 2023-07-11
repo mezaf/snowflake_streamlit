316abad6d8acf8064974a55e79890bd3ace31d73
Fernando Meza <43585972+mezaf@users.noreply.github.com>
2023-07-11 14:26:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
-fruit_choice = streamlit.text_input('What fruit would you like to add')
+fruit_choice = streamlit.text_input('What fruit would you like to add','jackfruit')
 try:
   my_cur.execute(f"insert into fruit_load_list (fruit_name) values ({fruit_choice})")
   my_cur.execute("commit")
