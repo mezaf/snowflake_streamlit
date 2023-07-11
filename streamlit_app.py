46557c4cc8f822432642ddce2a6c2b48ea06497a
Fernando Meza <43585972+mezaf@users.noreply.github.com>
2023-07-11 14:28:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice = streamlit.text_input('What fruit would you like to add','jackfruit')
 try:
-  my_cur.execute(f"insert into fruit_load_list values ({fruit_choice})")
+  my_cur.execute(f"insert into fruit_load_list values ('{fruit_choice}')")
   my_cur.execute("commit")
 except Exception as e:
   raise e
